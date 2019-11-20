2b76874f6d92d7c198ed966233fd512e0aad8a99
willy <willy@hngf.com.tw>
2019-11-20 17:37:13 +0800

    initial commit

---
+++ app/src/main/java/com/example/sideproject/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.sideproject.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
